8809a8a0964d8778519962ba919f864c67f5e3e0
khadija hussain <hussainkhadija8@gmail.com>
2020-03-27 05:36:07 +0500

    routes and middleware

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  console.log("this always run first");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in add product firmware");
+  res.send("<h1>hello from add product</h1>"); //send a response
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in express firmware");
+  res.send("<h1>hello from express</h1>"); //send a response
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
+//incoming req
+
+//next is funtion to travel req to other layer
+
 //if more modules
 // const server = http.createServer(routes.handler,routes.second );
-
 //take request listener as arg which is function
 // execute for every incoming request
 //   console.log(req.url, req.method, req.headers);
 //req of incoming request node object
 //process.exit();
-
-server.listen(3000);
-//incoming req
