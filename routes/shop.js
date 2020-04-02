e037c796ca02f6f4219f528350b7bdca8e651a35
khadija hussain <hussainkhadija8@gmail.com>
2020-04-02 05:28:02 +0500

    dynamic output

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,16 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // const products = adminData.products;
-  // res.render("shop", { prods: products });
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+  //res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
